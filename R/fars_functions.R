6083eac068febde1fec9749b3a25fb8decdbd9b6
Márk PADOS <mark.pados@xapt.com>
2018-05-17 16:38:28 +0200

    .

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -19,7 +19,7 @@ fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
   data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
+    readr::read_csv(system.file("extdata", filename, package="buildingpackagefars"), progress = FALSE)
   })
   dplyr::tbl_df(data)
 }

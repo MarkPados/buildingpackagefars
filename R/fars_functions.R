933fc923765d987b05ea9b6177f5347343687c39
Márk PADOS <mark.pados@xapt.com>
2018-05-18 15:25:44 +0200

    .

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -101,7 +101,7 @@ fars_read_years <- function(years) {
 #'
 #' @export
 fars_summarize_years <- function(years) {
-  library(dplyr)
+  require(dplyr)
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
     dplyr::group_by(year, MONTH) %>%
